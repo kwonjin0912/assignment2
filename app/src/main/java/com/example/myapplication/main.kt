5663f545b0595d48c41ebbe52f7dd464b30496c9
권진 <kwonjin0912@gmail.com>
2025-03-30 02:27:19 +0900

    git commit

--- app/src/main/java/com/example/myapplication/main.kt
+++ app/src/main/java/com/example/myapplication/main.kt
@@ -5,20 +5,29 @@ private fun <T> Set<T>.skip(n: Int): List<T>
     return this.withIndex().filter { it.index % n == 0 }.map { it.value }
 }
 
-sealed class BcsdLabUser(var name : String, var emoji : String){
+open class BcsdLabUser(var name : String, var emoji : String){
 
-    class Beginner(name: String, val assignment : Int) : BcsdLabUser(name, "새싹")
-    class Regular(name: String, val pay: Boolean) : BcsdLabUser(name, "사과")
-    class Mentor(name: String, val email: String ?= null) : BcsdLabUser(name, "별")
+    open fun printwhat(){
+        println("${name} ${emoji}")
+    }
 }
-open class Beginner(){
-    var assignment : Int = 0
+class Beginner(name: String, val assignment1 : Int) : BcsdLabUser(name, "새싹"){
+    var assignment : Int = assignment1
+    override fun printwhat(){
+        println("비기너 = ${name} ${emoji} - 과제 안한 횟수 : ${assignment}")
+    }
 }
-open class Regular(){
-    var pay : Boolean = true
+open class Regular(name: String, val pay1: Boolean) : BcsdLabUser(name, "사과"){
+    var pay : Boolean = pay1
+    override fun printwhat(){
+        println("레귤러 = ${name} ${emoji} - 이번달 회비를 냄: ${pay}")
+    }
 }
-open class Mentor(){
-    var email : String? = null
+open class Mentor(name: String, val email1: String ?= null) : BcsdLabUser(name, "별"){
+    var email : String? = email1
+    override fun printwhat(){
+        println("멘토 = ${name} ${emoji} - 이메일 : ${email ?: "없음"}")
+    }
 }
 fun main() {
     val list1 = listOf(1, 2, 3)
@@ -32,38 +41,33 @@ fun main() {
     println(result)
 
     val users: List<BcsdLabUser> = listOf(
-        BcsdLabUser.Beginner("권진", 3),
-        BcsdLabUser.Beginner("김보성", 0),
-        BcsdLabUser.Regular("김도훈", false),
-        BcsdLabUser.Regular("김상진", true),
-        BcsdLabUser.Mentor("김영찬", "zerochan@naver.com"),
-        BcsdLabUser.Mentor("양석현") // 이메일 없는 멘토
+        Beginner("권진", 3),
+        Beginner("김보성", 0),
+        Regular("김도훈", false),
+        Regular("김상진", true),
+        Mentor("김영찬", "zerochan@naver.com"),
+        Mentor("양석현") // 이메일 없는 멘토
     )
     print("\n")
-    for (user in users)
-    {
-        when (user)
-        {
-            is BcsdLabUser.Beginner -> println("비기너 = ${user.name} ${user.emoji} - 과제 안한 횟수 : ${user.assignment}")
-            is BcsdLabUser.Regular -> println("레귤러 = ${user.name} ${user.emoji} - 이번달 회비를 냄: ${user.pay}")
-            is BcsdLabUser.Mentor -> println("멘토 = ${user.name} ${user.emoji} - 이메일 : ${user.email ?: "없음"}")
-        }
+    for (user in users) {
+        user.printwhat()
     }
-    val beginners = users.filterIsInstance<BcsdLabUser.Beginner>()
+
+    val beginners = users.filterIsInstance<Beginner>()
 
     println("\n모든 비기너들:")
     for (beginner in beginners) {
         println("${beginner.name} ${beginner.emoji}")
     }
 
-    val beginnerassignment = users.filterIsInstance<BcsdLabUser.Beginner>().filter { it.assignment >= 3 }
+    val beginnerassignment = users.filterIsInstance<Beginner>().filter { it.assignment >= 3 }
 
     println("\n과제를 3회 이상 수행하지 못한 비기너들:")
     for (beginner in beginnerassignment) {
         println("${beginner.name} ${beginner.emoji}")
     }
 
-    val regularpay = users.filterIsInstance<BcsdLabUser.Regular>().filter { it.pay == false }
+    val regularpay = users.filterIsInstance<Regular>().filter { it.pay == false }
     println("\n이번달 회비를 내지 않은 레귤러들:")
     for (regular in regularpay){
         println("${regular.name} ${regular.emoji}")
@@ -76,6 +80,3 @@ fun main() {
     }
 
 }
-
-
-
